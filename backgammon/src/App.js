afcdf921b11c36548ca0ec096533689b930e05b1
Luka Licheli <lukalicheli@gmail.com>
2023-04-12 00:59:27 -0700

    added the board to the center of the page. It is positioned where i want it at this point to start the gridding process

--- backgammon/src/App.js
+++ backgammon/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import board from "./assets/backgammon.jpg";
 
 function App() {
-  return <div className="game-container"></div>;
+  return (
+    <div className="game-container">
+      <div className="backgammon-board">
+        <img className="board-image" src={board}></img>
+      </div>
+    </div>
+  );
 }
 
 export default App;
