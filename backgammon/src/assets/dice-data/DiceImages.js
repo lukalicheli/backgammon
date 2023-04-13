ca2b0b79b2102e0e390fe2f903beeb5dad3bb81d
Luka Licheli <lukalicheli@gmail.com>
2023-04-12 17:37:04 -0700

    Created a dice images and piece images objects with their respective images so i can use them in components

---
+++ backgammon/src/assets/dice-data/DiceImages.js
@@ -0,0 +1,35 @@
+import dice1 from "./dice1.png";
+import dice2 from "./dice2.png";
+import dice3 from "./dice3.png";
+import dice4 from "./dice4.png";
+import dice5 from "./dice5.png";
+import dice6 from "./dice6.png";
+
+export const diceImages = [
+  {
+    value: 1,
+    img: dice1,
+  },
+  {
+    value: 2,
+    img: dice2,
+  },
+  {
+    value: 3,
+    img: dice3,
+  },
+  {
+    value: 4,
+    img: dice4,
+  },
+  {
+    value: 5,
+    img: dice5,
+  },
+  {
+    value: 6,
+    img: dice6,
+  },
+];
+
+export default diceImages;
---
+++ backgammon/src/assets/piece-data/PieceImages.js
@@ -0,0 +1,15 @@
+import brown from "./brown-piece.png";
+import white from "./white-piece.png";
+
+export const pieceImages = [
+  {
+    value: brown,
+    img: brown,
+  },
+  {
+    value: white,
+    img: white,
+  },
+];
+
+export default pieceImages;
