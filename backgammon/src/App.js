ada48745b7d10b998a657dbc12ff84eeaf4318d2
Luka Licheli <lukalicheli@gmail.com>
2023-04-13 03:08:56 -0700

    Making headway with the gridding for the board

--- backgammon/src/App.js
+++ backgammon/src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
-import board from "./assets/backgammon.jpg";
+import brown from "./assets/piece-data/brown-piece.png";
 
 function App() {
+  const [board, setBoard] = useState(Array(24).fill(brown));
+
+  const handleClick = (e) => {};
+
   return (
     <div className="game-container">
-      <div className="backgammon-board"></div>
+      <div className="backgammon-board">
+        {board.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className={`column ${value}`}
+              onClick={() => handleClick(index)}
+            >
+              <img className="gammon-piece" src={value} alt="piece" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
