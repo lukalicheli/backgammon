0440de36b967deb5b510b3546967ee1b0aabdb70
Luka Licheli <lukalicheli@gmail.com>
2023-04-14 01:50:32 -0700

    Now I am getting integers in every column. Next step is to make sure the mapping is correct and then to eventually display an image of a piece times the integer inside of the array

--- backgammon/src/App.js
+++ backgammon/src/App.js
@@ -4,20 +4,46 @@ import brown from "./assets/piece-data/brown-piece.png";
 
 function App() {
   const [board, setBoard] = useState(Array(24).fill(brown));
+  const [backgammon, setBackgammon] = useState([
+    [2, 0],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [5, 1],
+    [0, -1],
+    [3, 1],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [5, 0],
+    [5, 1],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [3, 0],
+    [0, -1],
+    [5, 0],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [0, -1],
+    [2, 1],
+  ]);
 
   const handleClick = (e) => {};
 
   return (
     <div className="game-container">
       <div className="backgammon-board">
-        {board.map((value, index) => {
+        {backgammon.map((value, index) => {
           return (
             <div
               key={index}
               className={`column ${value}`}
               onClick={() => handleClick(index)}
             >
-              <img className="gammon-piece" src={value} alt="piece" />
+              {value[0]}
             </div>
           );
         })}
